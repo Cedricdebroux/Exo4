e324b3984b5653a958bb39bed5c44adb5eedc8bd
Cedric Debroux <cedric@debroux.ovh>
2022-08-17 12:37:27 +0200

    init

---
+++ Exo4/Models.swift
@@ -0,0 +1,23 @@
+//
+//  Models.swift
+//  Exo4
+//
+//  Created by Cédric Debroux on 17/08/2022.
+//
+
+import Foundation
+import UIKit
+
+struct Hero {
+    var name: String
+    var image: UIImage
+}
+
+struct Color {
+    var colorName: String
+    var backGroundColor: UIColor
+}
+
+struct Story {
+    var story: String
+}
